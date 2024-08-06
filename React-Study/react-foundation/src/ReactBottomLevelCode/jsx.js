d6ec4072d4164d473bddb4a36cbea08c60e9e520
yangDaXia <2215890610@qq.com>
2024-08-06 16:29:43 +0800

    增加md

--- React-Study/react-foundation/src/ReactBottomLevelCode/jsx.js
+++ React-Study/react-foundation/src/ReactBottomLevelCode/jsx.js
@@ -13,8 +13,12 @@ const ElementCreate = ()=> React.createElement('h1',{
     className:'box_h1'
 },'我创建了一个元素')
 
+//1、虚拟DOM，本质就是Object类型
+//2、虚拟DOM属性值少 轻，真实的DOM属性多 重。虚拟DOM是React内部在用，无需真实 DOM上那么多属性
+//3、
 const ReactCreateElementDom = {
     type: 'h1',
+    ref:null,
     props:{
         className:'box_h1',
         children: '我创建了一个元素'
